03e5a5d33399a9c072f6adece9963890393ec047
Rikard Fröberg <rikard@morus.se>
2017-04-10 14:20:06 +0200

    Some more room strings

--- se/itu/game/test/TestUtils.java
+++ se/itu/game/test/TestUtils.java
@@ -14,52 +14,53 @@ import java.util.ArrayList;
 
 public class TestUtils {
 
-    public static final String START_ROOM_DESCR = "You are standing at the end of";
-    public static final String EAST_ROOM_DESCR = "You are inside a building";
-    public static final String WEST_ROOM_DESCR = "You have walked up a hill";
-    public static final String NORTH_ROOM_DESCR = "You are in open forest";
-
-    private static Room createRoom(String description,
-                                   Room north,        
-                                   Room east,
-                                   Room south,        
-                                   Room west) {
-        List<Thing> things = new ArrayList<Thing>();
-        return new Room (description,north,east,south,west, things);
-    }
+  public static final String START_ROOM_DESCR = "You are standing at the end of";
+  public static final String EAST_ROOM_DESCR = "You are inside a building";
+  public static final String WEST_ROOM_DESCR = "You have walked up a hill";
+  public static final String NORTH_ROOM_DESCR = "You are in open forest";
+  public static final String SOUTH_4_ROOM_DESCR = "You are in a small chamber";
+  public static final String CAGE_ROOM_DESC = "You are crawling over cobbles";
+  private static Room createRoom(String description,
+                                 Room north,        
+                                 Room east,
+                                 Room south,        
+                                 Room west) {
+    List<Thing> things = new ArrayList<Thing>();
+    return new Room (description,north,east,south,west, things);
+  }
     
 
-    public static Room getTest1Cave()  {
+  public static Room getTest1Cave()  {
 
-        Room startingRoom = createRoom("You are standing at the end of .....",
-                                       null, null, null, null);
-        
-        Room westRoom = createRoom("You have walked up a hill ...",
+    Room startingRoom = createRoom("You are standing at the end of .....",
                                    null, null, null, null);
         
-        Room northRoom = createRoom("You are in open forest ....",
-                                    null, null, null, null);
+    Room westRoom = createRoom("You have walked up a hill ...",
+                               null, null, null, null);
         
-        Room southRoom = createRoom("You are in a valley ....",
-                                   null, null, null, null);
+    Room northRoom = createRoom("You are in open forest ....",
+                                null, null, null, null);
         
-        List<Thing> things = new ArrayList<Thing>();
-        things.add(new Thing("Skeleton key"));
-        Room eastRoom = new Room("You are inside a building, .....",
-                                 null, null, null, null, things);
+    Room southRoom = createRoom("You are in a valley ....",
+                                null, null, null, null);
         
-        startingRoom.setConnectingRoom(EAST, eastRoom);
-        startingRoom.setConnectingRoom(WEST, westRoom);
-        startingRoom.setConnectingRoom(NORTH,northRoom);
-        startingRoom.setConnectingRoom(SOUTH, southRoom);
+    List<Thing> things = new ArrayList<Thing>();
+    things.add(new Thing("Skeleton key"));
+    Room eastRoom = new Room("You are inside a building, .....",
+                             null, null, null, null, things);
         
-        eastRoom.setConnectingRoom(WEST, startingRoom);
-        westRoom.setConnectingRoom(EAST, startingRoom);
-        southRoom.setConnectingRoom(NORTH, startingRoom);
-        northRoom.setConnectingRoom(SOUTH, startingRoom);
+    startingRoom.setConnectingRoom(EAST, eastRoom);
+    startingRoom.setConnectingRoom(WEST, westRoom);
+    startingRoom.setConnectingRoom(NORTH,northRoom);
+    startingRoom.setConnectingRoom(SOUTH, southRoom);
         
-        return startingRoom;
-    }
+    eastRoom.setConnectingRoom(WEST, startingRoom);
+    westRoom.setConnectingRoom(EAST, startingRoom);
+    southRoom.setConnectingRoom(NORTH, startingRoom);
+    northRoom.setConnectingRoom(SOUTH, startingRoom);
+        
+    return startingRoom;
+  }
 
 
 }
