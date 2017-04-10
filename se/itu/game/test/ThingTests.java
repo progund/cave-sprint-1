908f1b48151a37df84c75cdea7136c09918afab4
Rikard Fröberg <rikard@morus.se>
2017-04-10 14:05:17 +0200

    Changed it to use the Things class instead

--- se/itu/game/test/ThingTests.java
+++ se/itu/game/test/ThingTests.java
@@ -4,6 +4,7 @@ import se.itu.game.cave.Room;
 import se.itu.game.cave.Player;
 import se.itu.game.cave.Thing;
 import se.itu.game.cave.init.CaveInitializer;
+import se.itu.game.cave.init.Things;
 
 import static se.itu.game.test.TestUtils.START_ROOM_DESCR;
 import static se.itu.game.test.TestUtils.EAST_ROOM_DESCR;
@@ -17,6 +18,9 @@ import static se.itu.game.cave.Room.Direction.WEST;
 
 public class ThingTests {
 
+  private Player player = Player.getInstance();
+  private Thing key = Things.get("Skeleton Key");
+
   private void testT1() {
         
     /*
@@ -27,20 +31,18 @@ public class ThingTests {
       Verify that the key is missing in the room’s list of things.
       Verify that the key is present in the player’s inventory.
     */
-    CaveInitializer.getInstance().initAll();
-    Player player = Player.getInstance();
-    Thing key = new Thing("Skeleton Key");
     System.out.println("Current room: " + player.currentRoom());
     player.go(EAST);
     assert(player.currentRoom().description().startsWith(EAST_ROOM_DESCR));
-    assert(!player.currentRoom().things().contains(new Thing("Random crap"))) : "Unexpected object found";
+    assert(player.currentRoom().things().size() == 1)
+      : "Room should only have one thing, had: " + player.currentRoom().things();
+      
     assert(player.currentRoom().things().contains(key)) : "Could not find Skeleton key in east room ";
 
     player.takeThing(key);        
     assert(!player.currentRoom().things().contains(key)) : "Could wrongly find Skeleton key in east room ";
     assert(player.inventory().contains(key)) : "Could not find Skeleton key in player's inventory ";
   }
-
 
   private void testT2() {
         
@@ -51,26 +53,21 @@ public class ThingTests {
       Verify that the key is present in the room’s list of things.
       Verify that the key is missing in the player’s inventory.
     */
-    CaveInitializer.getInstance().initAll();
-    Player player = Player.getInstance();
-    Thing key = new Thing("Skeleton Key");
-
     player.dropThing(key);
     assert(player.currentRoom().things().contains(key)) : "Could not find Skeleton key in east room ";
     assert(!player.inventory().contains(key)) : "Could not find Skeleton key in player's inventory ";
   }
 
-
   public static void main(String [] args) {
 
+    CaveInitializer.getInstance().initAll();
     System.out.print("Test case T1: ");
-    new ThingTests().testT1();
+    ThingTests test = new ThingTests();
+    test.testT1();
     System.out.println("OK");
 
     System.out.print("Test case T2: ");
-    new ThingTests().testT2();
+    test.testT2();
     System.out.println("OK");
-
-  }
-    
+  }    
 }
